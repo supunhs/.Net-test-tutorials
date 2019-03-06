b14d2b50d1d585a5becada138fe836501d9c13f9
supunhs <supunkarunarathna92@gmail.com>
2019-03-06 14:19:10 +0530

    first commit

---
+++ WpfApp2/WpfApp2/ViewModel/ShellViewModel.cs
@@ -0,0 +1,31 @@
+﻿using WpfApp2;
+using WpfApp2.Models;
+using WpfApp2.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace WpfApp2.ViewModel
+{
+    public class ShellViewModel
+    {
+
+        //public List<PersonModel> People { get; set; }
+        public ObservableCollection<PersonModel> People { get; set; }
+
+        public ShellViewModel()
+        {
+            Console.WriteLine("With the default new line characters:");
+            DataAccess da = new DataAccess();
+            People = new ObservableCollection<PersonModel>(da.GetPeople());
+            Console.WriteLine(People.Count);
+            foreach (var item in People)
+            {
+                Console.WriteLine(item.FirstName.ToString());
+            }
+        }
+    }
+}
